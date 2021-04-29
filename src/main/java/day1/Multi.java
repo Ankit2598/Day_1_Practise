898ff97e428122dfc840196dd162ef436ad62d21
Ankit2598 <shank2598@gmail.com>
2021-04-29 21:55:27 +0530

    Pushing Test

---
+++ src/main/java/day1/Multi.java
@@ -0,0 +1,7 @@
+package day1;
+
+public class Multi {
+public static int mul(int num1, int num2){
+return num1*num2;
+}
+}
---
+++ src/test/java/day1/MultiTest.java
@@ -0,0 +1,12 @@
+package day1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+class MultiTest {
+
+ @Test
+void test() {
+assertEquals(2,Multi.mul(1,2));
+}
+
+}
